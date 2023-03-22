86a4fa9264e31444c2e5f07faedf84ebea8712dd
Soumalya Pakrashi <soumalyapakrashi@gmail.com>
2023-03-22 14:08:18 +0530

    Remove unused sections dealing with files for storage

--- models/Product.js
+++ models/Product.js
@@ -1,36 +1,6 @@
-const fs = require('fs');
-const path = require('path');
-
 const db = require('../utils/database');
 
 class Product {
-    static products = [];
-
-    constructor(title, picture, amount, description) {
-        Product.#readFile();
-        
-        Product.products.push({
-            id: Math.random().toString(),
-            title: title,
-            picture: picture,
-            amount: amount,
-            description: description
-        });
-
-        Product.#writeFile();
-    }
-
-    static #readFile() {
-        const file = fs.readFileSync(path.resolve(__dirname, '../data/products.json'), 'utf-8');
-        if(file !== '') {
-            Product.products = JSON.parse(file);
-        }
-    }
-
-    static #writeFile() {
-        fs.writeFileSync(path.resolve(__dirname, '../data/products.json'), JSON.stringify(Product.products));
-    }
-
     static addProduct(title, picture, amount, description) {
         return db.execute(
             'INSERT INTO product (title, picture, amount, description) VALUES (?, ?, ?, ?)',
