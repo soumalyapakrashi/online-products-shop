ce8dbe61c667fd9e1c2f45a484aa95c0747b3f82
Soumalya Pakrashi <soumalyapakrashi@gmail.com>
2023-05-02 07:59:05 +0530

    Fix authentication variable bug in error page

--- controllers/error.js
+++ controllers/error.js
@@ -1,5 +1,11 @@
 function error(request, response, next) {
-    response.status(404).render('error', { pageTitle: 'Not Found', activePage: 'Error' });
+    response.status(404).render('error', {
+        pageTitle: 'Not Found',
+        activePage: 'Error',
+        isAuthenticated: request.user ? true : false,
+        loggedInUsername: request.user ? request.user.name : undefined,
+        isAdminUser: true
+    });
 }
 
 module.exports = error;
