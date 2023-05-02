311fb8ff458a77f5c4b9545382765c14c83efb00
Soumalya Pakrashi <soumalyapakrashi@gmail.com>
2023-05-02 15:24:10 +0530

    Add CSRF token to all forms

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
 
 require('dotenv').config({
     path: path.resolve(__dirname, '.', '.env')
 });
+
+const csrfProtection = csrf();
 
 const shop_routes = require('./routes/shop');
 const admin_routes = require('./routes/admin');
@@ -41,6 +44,15 @@ app.use(express.static('./public'));
 
 // Set up the body parser for parsing forms
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// Register middleware for CSRF token
+app.use(csrfProtection);
+
+// Put the csrfToken variable in all the views
+app.use((request, response, next) => {
+    response.locals.csrfToken = request.csrfToken();
+    next();
+})
 
 // Add the logged in user to the request so that it can be accessed from other parts
 app.use((request, response, next) => {
